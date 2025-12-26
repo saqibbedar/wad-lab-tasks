d16f4931e74ae1c9edb122dc6a88a12a6aa48f09
saqibbedar <chandio.saqib.m@gmail.com>
2025-12-26 11:03:44 +0500

    fix: updated shop menu UI bug - when documnet was scrolling, height of the menu getting subtracted/clipped - It's now fixed with window.scrollTo, when menu is open first last scrolled position is saved in a variable and then scroll is set to 0, and as soon as menu is closed scroll position is set to previous position.

--- src/tasks/Lab10/public/project/handleTopBanner.js
+++ src/tasks/Lab10/public/project/handleTopBanner.js
@@ -16,14 +16,38 @@ topBanner.lastElementChild.addEventListener("click", () => {
 
 
 // handle menu
+let previousScrollYPosition; // preserve last scroll position
+
 document.querySelector(".shop-menu").addEventListener("click", (e)=>{
   let menu = document.querySelector(".menu");
   menu.classList.toggle("control-visibility");
   if(menu.classList.contains("control-visibility")) {
     e.target.textContent = "Close";
     document.body.style.overflow = "hidden";
+
+    // save the last location where user scrolled
+    previousScrollYPosition = window.scrollY || document.documentElement.scrollTop;
+    // console.log(previousScrollYPosition)
+
+    // scroll to top when user is exploring menu
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
   } else {
     e.target.textContent = "SHOP";
     document.body.style.overflow = "auto"
+
+    // console.log(previousScrollYPosition)
+    // send back to original position after menu exploration is done
+    if(previousScrollYPosition > 0) {
+      window.scrollTo({
+        top: previousScrollYPosition,
+        left: 0,
+        behavior: "smooth",
+      });
+      previousScrollYPosition = 0;
+    }
   }
 })
